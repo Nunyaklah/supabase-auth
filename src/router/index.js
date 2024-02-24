73cfb7b155069fea64409463648e59132970c22f
Nunyaklah <nunyaklah@gmail.com>
2024-02-24 10:49:24 +0000

    add authentication

--- src/router/index.js
+++ src/router/index.js
@@ -5,16 +5,22 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
+      path: "/home",
       name: "home",
       component: HomeView,
     },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   component: () => import('../views/AboutView.vue')
-    // }
+    {
+      path: "/sign-in",
+      name: "sign in",
+      component: () => import("../views/SignIn.vue"),
+    },
+    {
+      path: "/",
+      name: "sign up",
+      component: () => import("../views/SignUp.vue"),
+    },
   ],
 });
+
 
 export default router;
